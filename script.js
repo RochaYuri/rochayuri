f8f5cbe6f622e051b253818e6a6d36ed513e1bda
RochaYuri <91890056+RochaYuri@users.noreply.github.com>
2022-12-20 15:02:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var height = window.screen.height;
+var width = window.screen.width;
+
+const lb = document.querySelector(".lb");
+const cb2 = document.querySelector(".cb2");
+const rb = document.querySelector(".rb");
+const lm = document.querySelector(".lm");
+const rm = document.querySelector(".rm");
+const at2 = document.querySelector(".at2");
+const abilitiesSkillContainer = document.querySelector(".abilitiesSkillContent");
+const abilitiesContentContainer = document.querySelector(".abilitiesContentContainer")
+
+
+function trocaTexto() {
+    document.querySelector("#paragrafo").textContent = "Tetse";
+}
+
+if (width < 1200) {
+    abilitiesSkillContainer.remove();
+}
+
+if (width < 768) {
+    lb.remove();
+    rb.remove();
+    lm.remove();
+    rm.remove();
+}
+
+if (width < 480) {
+    cb2.remove();
+    at2.remove();
+    abilitiesSkillContainer.remove()
+    abilitiesContentContainer.remove();
+}
