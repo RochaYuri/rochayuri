251cca9a9849ebc172f127d11dab606ea935657f
RochaYuri <rochayuri12@gmail.com>
2022-12-21 11:13:35 -0300

    teste created

---
+++ teste-lingua/js.js
@@ -0,0 +1,16 @@
+var userLang = navigator.language || navigator.userLanguage;
+console.log("The language is: " + userLang);
+if (userLang == 'pt-BR') {
+    element.remove(text1);
+}
+
+//REMOVER
+var text1 = document.getElementById("text1")
+//IF
+element.remove(ITEM)
+
+//ADICIONAR
+var element = document.querySelector(".text")
+var text = document.createTextNode("TESTEEEEEEEEE")
+//IF
+element.appendChild(text)
