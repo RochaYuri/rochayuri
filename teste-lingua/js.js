574d8319966f2ac56f529c7b2205608be4e055ed
RochaYuri <rochayuri12@gmail.com>
2022-12-26 08:07:40 -0300

    new project added
    Added the first project at the gk position

--- teste-lingua/js.js
+++ teste-lingua/js.js
@@ -1,16 +1,15 @@
 var userLang = navigator.language || navigator.userLanguage;
+var element = document.querySelector(".text")
+var text = document.createTextNode("Este é o meu portfólio! O lugar onde busco mostrar minhas habilidades e ideias nos projetos que crio! Sou uma pessoa apaixonada por tecnologia, esportes (acho que será possível perceber a forma dos projetos apresentados abaixo.), família e jogos. Acredito que a tecnologia tem o poder necessário para mudar a vida das pessoas por meio do acesso à informação. Vá em frente! Aproveite o tempo que me visita e, se gostar, contacte-me!")
+var text1 = document.getElementById("text1")
 console.log("The language is: " + userLang);
 if (userLang == 'pt-BR') {
-    element.remove(text1);
+    element.replaceChild(text1, text);
+    element.appendChild(text);
 }
 
-//REMOVER
-var text1 = document.getElementById("text1")
-//IF
-element.remove(ITEM)
 
-//ADICIONAR
-var element = document.querySelector(".text")
-var text = document.createTextNode("TESTEEEEEEEEE")
-//IF
-element.appendChild(text)
+//var userLang = navigator.language || navigator.userLanguage;
+//var element = document.querySelector(".text")
+//var text = document.createTextNode("TESTEEEEEEEEE")
+//var text1 = document.getElementById("text1")
